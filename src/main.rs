0e2704105063b95701841444ffe945c3f5ee67be
Alex Whitney <adwhit@fastmail.com>
2017-01-21 17:23:42 +0000

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,12 @@ mod errors {
 
 use errors::*;
 
-use hyper::Client;
-use hyper::client::HttpConnector;
+use hyper::client::{Client, HttpConnector};
+use hyper::status::StatusCode;
 use hyper::Url;
 
 use futures::Future;
-use futures::future::ok;
+use futures::future::{ok, err};
 use futures::sync::mpsc;
 use futures::Sink;
 use futures::stream::Stream;
@@ -33,28 +33,36 @@ use html5ever::rcdom::{self, RcDom};
 use html5ever::Attribute;
 use html5ever::tendril::TendrilSink;
 
+use std::rc::Rc;
+
+type HttpClient = Client<HttpConnector>;
+
 quick_main!(run);
 
-const ROOT: &'static str = "http://www.gutenberg.org";
-
 fn run() -> Result<()> {
-    let mut core = tokio_core::reactor::Core::new().chain_err(|| "Failed to open reactor")?;
+    let root = std::env::args().nth(1).ok_or(ErrorKind::Msg(String::from("No URL provided")))?;
+    let root = hyper::Url::parse(&root).chain_err(|| "Url failed to parse")?;
+    let mut core = reactor::Core::new().chain_err(|| "Failed to open reactor")?;
     let handle = core.handle();
 
 
     let client = Client::new(&handle);
     let (tx, rx) = mpsc::channel(1024);
-    // start root
-    start_worker(&handle, &tx, &client, vec![String::from(ROOT)]);
+    // get root
+    start_worker(&handle, &tx, &client, vec![root]);
 
     let mut ct = 0;
 
-    let handler = rx.for_each(move |links| {
+    let handler = rx.for_each(move |(page_url, links)| {
+        let linkurls = links.iter().filter_map(|link| {
+            hyper::Url::parse(&link).ok()
+        }).collect::<Vec<_>>();
+
         ct += 1;
-        println!("{:?}", links);
+        println!("Found {} links in {}", linkurls.len(), page_url );
         println!("COUNT: {}", ct);
         if ct < 5 {
-            start_worker(&handle, &tx, &client, links);
+            start_worker(&handle, &tx, &client, linkurls);
         }
         ok(())
     });
@@ -63,49 +71,55 @@ fn run() -> Result<()> {
     Ok(())
 }
 
-    fn start_worker(handle: &reactor::Handle,
-                    tx: &mpsc::Sender<Vec<String>>,
-                    client: &Client<HttpConnector>,
-                    links: Vec<String>) {
-    // start the root worker
-    for link in links {
-        let url = match hyper::Url::parse(&link) {
-            Ok(url) => url,
-            Err(_) => {
-                println!("Invalid URL: {}", link);
-                continue
-            }
-        };
+fn start_worker(handle: &reactor::Handle,
+                tx: &mpsc::Sender<(Url, Vec<String>)>,
+                client: &HttpClient,
+                links: Vec<Url>) {
+    for url in links {
         let tx = tx.clone();
-        let worker = fetch_html(&client, url)
-            .map(|buf| process(&buf))
-            .map_err(|_| ())
-            .and_then(move |links| tx.send(links)
-                    .then(|_| ok(())));
+        let worker = fetch_html(&client, url.clone())
+            .then(|res| {
+                match res {
+                    Ok((StatusCode::Ok, body)) => ok((url, process_html(&body))),
+                    Ok((status, _)) => {
+                        println!("Bad status code {} for {}", status, url);
+                        err(())
+                    },
+                    Err(e) => {
+                        println!("Worker error: {} for {}", e, url);
+                        err(())
+                    }
+                }
+            })
+            .and_then(|(url, links)| tx.send((url, links))
+                      .then(|_| ok(())));
         handle.spawn(worker);
     }
 }
 
 
-fn fetch_html(client: &Client<HttpConnector>, url: Url) -> impl Future<Item=Vec<u8>, Error=hyper::Error> {
+fn fetch_html(client: &HttpClient, url: Url) -> impl Future<Item=(StatusCode, Vec<u8>), Error=hyper::Error> {
     println!("Fetching URL: {}", url);
     client.clone()
         .get(url)
         .and_then(|res| {
-            let buf = {
+
+            let status = *res.status();
+
+            let bodybuf = {
                 let headers = res.headers();
                 let buflen = headers.get::<hyper::header::ContentLength>();
                 Vec::with_capacity(buflen.map(|b| b.0 as usize).unwrap_or(1024))
             };
 
-            res.body().fold(buf, |mut buf, chunk| {
-                buf.extend_from_slice(&*chunk);
-                ok::<_, hyper::Error>(buf)
-            })
+            res.body().fold(bodybuf, |mut bodybuf, chunk| {
+                bodybuf.extend_from_slice(&*chunk);
+                ok::<_, hyper::Error>(bodybuf)
+            }).map(move |b| (status, b))
         })
 }
 
-fn process(source: &[u8]) -> Vec<String> {
+fn process_html(source: &[u8]) -> Vec<String> {
     let dom = parse_html(source);
     find_links(&dom)
 }
